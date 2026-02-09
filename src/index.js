7e3c8e0ea6de39402de4a686eea144b54149ed00
Mostafa Hussien <mabozeed70@gmail.com>
2026-02-09 22:21:37 +0200

    fixed https to be http

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ const {broadcastMatchCreated} = attachWebSocketServer(server);
 app.locals.broadcastMatchCreated = broadcastMatchCreated;
 
 server.listen(PORT, HOST, () => {
-    const baseUrl = HOST === '0.0.0.0' ? `http://localhost:${PORT}` : `https://${HOST}:${PORT}`;
+    const baseUrl = HOST === '0.0.0.0' ? `http://localhost:${PORT}` : `http://${HOST}:${PORT}`;
 
     console.log(`Server is running on ${baseUrl}`);
     console.log(`WebSockets is running on ${baseUrl.replace("http", "ws")}/ws`)
